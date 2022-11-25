ad7c01c14f3a7a41412186be683885bf7245ca8d
Imam Abubakar <imamabubakar2002@gmail.com>
2022-11-25 01:39:30 +0000

    landing changes

--- interface/src/components/Landing/DemoButton.jsx
+++ interface/src/components/Landing/DemoButton.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DemoButton = () => {
   return (
     <React.Fragment>
+      <Link to="/login">
       <button
         className="button demo-button mb-5"
         style={{
           fill: "blue",
         }}
       >
-        View Demo
+        Get Started
       </button>
+      </Link>
     </React.Fragment>
   );
 };
